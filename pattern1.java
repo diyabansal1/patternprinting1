dc3150e1b72ac7709fa9ebab4bab2d269526adec
Diya <diya.bansal_cs21@gla.ac.in>
2023-02-12 12:00:00 +0530

    pattern

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+public class pattern1 {
+    public static void main(String[] args){
+        int n=5;
+        int row=1;
+        int star=n;
+        while(row<=n){
+            //star
+            int i=1;
+            while(i<=star){
+                System.out.print("*"+" ");
+                i++;
+            }
+            row++;
+            System.out.println();
+        }
+    }
+}
---
+++ pattern2.java
@@ -0,0 +1,20 @@
+public class pattern2 {
+
+    public static void main(String[] args){
+        int n=5;
+        int row=1;
+        int star=1;
+        while(row<=n){
+            //star
+            int i=1;
+            while(i<=star){
+                System.out.print("*"+" ");
+                i++;
+            }
+            row++;
+            star++;
+            System.out.println();
+        }
+    }
+}
+
